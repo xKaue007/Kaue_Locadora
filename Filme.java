3485ec1a8ad200db7791eb6dcf21784be150d1d9
xKaue007 <kaueoliveira92@gmail.com>
2021-09-23 18:39:00 +0000

    Comentado

--- Filme.java
+++ Filme.java
@@ -1,17 +1,27 @@
 public class Filme {
+	//int a pessoa adicionara um numero inteiro como codigo de algum filme
 	private int codigo;
+
+	//string adicionara um nome de filme no formato de texto
 	private String nome;
+
+	//doble adicionara o valor do filme com as casas decimais
 	private double valor;
+	
+	//se esta disponivel ou não
 	private boolean disponivel;
 
+	//pegará o valor inserido em codigo
 	public int getCodigo() {
 		return codigo;
 	}
+
 
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
 
+	//pegará o texto inserido em nome
 	public String getNome() {
 		return nome;
 	}
@@ -19,7 +29,8 @@ public class Filme {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	//pegará o valor inserido em valor
 	public double getValor() {
 		return valor;
 	}
@@ -28,6 +39,7 @@ public class Filme {
 		this.valor = valor;
 	}
 
+	//mostrará se esta ou não disponivel
 	public boolean isDisponivel() {
 		return disponivel;
 	}
@@ -36,6 +48,8 @@ public class Filme {
 		this.disponivel = disponivel;
 	}
 
+	//caso teha o filme mostrará true e sara como disponivel
+	//casso de false, irá para else e exibira que não esta disponivel
 	public void retirar() {
 		if(disponivel == true) {
 			disponivel = false;
@@ -49,6 +63,7 @@ public class Filme {
 		disponivel = true;
 	}
 	
+	//exibirá (ira imprimir) tudo que foi pesquisado
 	public void mostra(){
 		System.out.println("Código do filme:" + this.codigo);
 		System.out.println("Nome do filme:" + this.nome);
--- Programa.java
+++ Programa.java
@@ -1,11 +1,25 @@
+//nome da classe
 public class Programa {
+
+	// definindo o metodo MAIN
 	public static void main(String[] args) {
+
+		//puxando a aba filme
 		Filme f = new Filme();
+
+		//pegar o valor inseirdo no caso "Codigo"
 		f.setCodigo(123);
+
+		//pegar o valor inseirdo no caso "Nome do Filme"
 		f.setNome("A Era do Gelo");
+
+		//pegar o valor inseirdo no caso "Valor"
 		f.setValor(2.50);
+
+		//pegar o valor inseirdo no caso "se está disponivel ou não"
 		f.devolver();
 		
+		//pegar o valor inseirdo no caso "imprimir tudo que o cliente pediu"
 		f.mostra();
 		
 		f.retirar();
